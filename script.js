1d7569d781e1fe2562d5ac5647a606e888b461bb
Dagyula <dagyula@users.noreply.github.com>
2020-04-11 21:57:39 +0200

    correction

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let value = 0
+let val=0
 $("h1").append ("<h2>"+value+"</h2>")
 $("#increase").click(() => {
     value = value + 1
     console.log(value)
-     if (value === 3) {
+     if (value === 3 && val!==3) {
         console.log("its 3")
+        val = 3
         $('#thediv').append('<img id="theImg" src="./gyula.gif"/>')
         $('#divsecond').append('<img id="theImgsecond" height="200" width="400" src="./bazinga.jpg"/>')}
-    $("h2").text(value)
+        $("h2").text(value)
 } )
 $("#decrease").click(()=>
 {
